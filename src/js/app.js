8c46da733240fe7d0a8c73ce6ea5a803a81729dd
cfen <chris.fenn@guardian.co.uk>
2018-04-09 18:36:25 +0100

    basic rollover on list showing biogs

--- src/js/app.js
+++ src/js/app.js
@@ -5,27 +5,28 @@ import Handlebars from 'handlebars/dist/handlebars'
 
 import listTemplate from "../templates/list.html"
 
-function init(){
 
-	axios.get(`${process.env.PATH}/assets/data/grenfell-test-data.json`).then((resp) => {
-		buildView(resp.data);
-	});
+var previousSelection;
 
-   
+function init() {
+    axios.get(`${process.env.PATH}/assets/data/grenfell-test-data.json`).then((resp) => {
+        buildView(resp.data);
+    });
+
+
 }
 
 
-function buildView(data){
-
-		const listHTML = compileListHTML(data);
-
-		document.querySelector('.gren-list-wrapper').innerHTML = listHTML;
+function buildView(data) {
+    const listHTML = compileListHTML(data);
+    document.querySelector('.gren-list-wrapper').innerHTML = listHTML;
+    addListeners();
 }
 
 
 function compileListHTML(dataIn) {
 
-	console.log(dataIn)
+    console.log(dataIn)
 
     Handlebars.registerHelper('html_decoder', function(text) {
         var str = unescape(text).replace(/&amp;/g, '&');
@@ -44,6 +45,30 @@ function compileListHTML(dataIn) {
 
     return newHTML
 
+}
+
+function addListeners() {
+    document.querySelectorAll(".gren-list-name").forEach((el) => {
+        el.addEventListener('mouseover', function() { showLinkedInfo(this.getAttribute("key-ref")) });
+    })
+}
+
+function showLinkedInfo(n) {
+
+	if (previousSelection){
+		previousSelection.classList.add("hide");
+	}
+
+	document.querySelectorAll(".gren-list-biog").forEach((el) => {
+		let nn = el.getAttribute("key-ref");
+		console.log(el)
+		if(n == nn){
+			el.classList.remove("hide");
+			previousSelection = el;
+
+		}
+
+	})
 }
 
 
